e06c19528f0ae780597a944f7ccb6feaea238781
gannash <mbinyaminov@gmail.com>
2018-12-15 22:46:59 +0200

    added routing and preview page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
-import SearchUserTextBox from './components/SearchUserTextBox/SearchUserTextBox';
-import RecentSearchedUsers from './components/RecentSearchedUsers/RecentSearchedUsers';
+// import SearchUserTextBox from './components/SearchUserTextBox/SearchUserTextBox';
+// import RecentSearchedUsers from './components/RecentSearchedUsers/RecentSearchedUsers';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './views/Home';
+import Preview from './views/Preview';
 
 class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <div className='heading'>
-          <div className='logo-image-container'>
-            <img className='logo-image' src={logo} alt='logo'></img>
+      <Router>
+        <div className='app'>
+          <div className='heading-container'>
+            <div className='heading'>
+              <div className='logo-image-container'>
+              <Link to="/"><img className='logo-image' src={logo} alt='logo'></img></Link>
+              </div>
+              <div>
+                <ul className='main-menu' style={{ listStyleType: 'none' }}>
+                  <li><Link className='menu-link-item' to="/about">עלינו</Link></li>
+                  <li><Link className='menu-link-item' to="/pricing">מחירון</Link></li>
+                  <li><Link className='menu-link-item' to="/popular">חיפושים נפוצים</Link></li>
+                </ul>
+              </div>
+              {/* <div>USER</div> */}
+            </div>
           </div>
-          <div>
-            <ul className='main-menu' style={{ listStyleType: 'none' }}>
-              <li>עלינו</li>
-              <li>מחירון</li>
-              <li>חיפושים נפוצים</li>
-            </ul>
+          {/* <div id='check-user' style={{ display: 'flex', flexDirection: 'column' }}>
+            <h3 style={{ fontSize: '30px', fontWeight: '100', width: '600px' }}>בדקו כמה עוקבים מזוייפים יש למשתמשי אינסטגרם באמצעות מנגנון חכם ומתקדם</h3>
+            <div style={{ width: '600px', marginBottom: '200px' }}>
+              <SearchUserTextBox />
+            </div>
           </div>
-          {/* <div>USER</div> */}
+          <RecentSearchedUsers /> */}
+          <div className='app-body'>
+            <Route exact path="/" component={Home} />
+            {/* <Route path="/about" component={About} /> */}
+            {/* <Route path="/pricing" component={Topics} /> */}
+            {/* <Route path="/popular" component={Topics} /> */}
+            <Route path="/preview" component={Preview} />
+          </div>
         </div>
-        <div id='check-user' style={{ display: 'flex', flexDirection: 'column' }}>
-          <h3 style={{ fontSize: '42px' }}>בדוק כמה עוקבים מזוייפים יש למשתמשי אינסטגרם</h3>
-          <div style={{ width: '500px', marginBottom: '200px' }}>
-            <SearchUserTextBox />
-          </div>
-        </div>
-        <RecentSearchedUsers />
-      </div>
+      </Router>
     );
   }
 }
