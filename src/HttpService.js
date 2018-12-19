c65328528a470d93cbbbf3f2d4cc8065bfdcc565
gannash <mbinyaminov@gmail.com>
2018-12-19 21:42:28 +0200

    added production api url

--- src/HttpService.js
+++ src/HttpService.js
@@ -1,4 +1,8 @@
-const defaultUrl = 'http://localhost:3001';
+let defaultUrl = 'http://localhost:3001';
+
+if(process.env.NODE_ENV === 'production') {
+    defaultUrl = 'https://instalyzer-api.herokuapp.com';
+}
 
 export const _fetch = function (path, init) {
     return fetch(defaultUrl + path, init);
