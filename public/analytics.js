20cdf2eab240b0ade1f647c76e495d0a07d971e3
Yonatan Kreiner <yonatankreiner@gmail.com>
2019-01-14 11:21:51 +0200

    move analytics out of index.html

---
+++ public/analytics.js
@@ -0,0 +1,7 @@
+if ('%NODE_ENV%' === 'production') {
+	window.dataLayer = window.dataLayer || [];
+	function gtag() { window.dataLayer.push(arguments); }
+	gtag('js', new Date());
+
+	gtag('config', 'UA-132382162-1');
+}
